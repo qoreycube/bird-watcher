c503f33173fbaacb3092f1e0975c32071673dccb
Cory Kratz <qoreycube@gmail.com>
2025-08-08 14:03:23 -0500

    feat(home): Testing out capturing a photo from the phone's camera app.

--- app/page.tsx
+++ app/page.tsx
@@ -57,11 +57,13 @@ export default function Home() {
         
         <form className="flex flex-col items-center gap-4 bg-gray-100 dark:bg-gray-800 p-6 rounded shadow-md">
           <label htmlFor="image-upload" className="text-lg font-medium text-gray-700 dark:text-gray-200">Select an image:</label>
+          
           <input
             type="file"
             id="image-upload"
             name="image"
             accept="image/*"
+            capture="environment"
             className="block w-full text-sm text-gray-500 file:mr-4 file:py-2 file:px-4 file:rounded file:border-0 file:text-sm file:font-semibold file:bg-blue-50 file:text-blue-700 hover:file:bg-blue-100"
             onChange={handleImageChange}
           />
